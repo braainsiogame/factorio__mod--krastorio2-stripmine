df425ac722853332b78ca30659c0f34b21245c38
Patrick 'Quezler' Mounier <Quezler@me.com>
2020-05-25 10:52:32 +0200

    Add files via upload

---
+++ Krastorio2_Stripmine_1.0.0/data-final-fixes.lua
@@ -0,0 +1,3 @@
+data.raw["mining-drill"][   "electric-mining-drill"    ].resource_searching_radius = (5 / 2) - 0.01;
+data.raw["mining-drill"]["kr-electric-mining-drill-mk2"].resource_searching_radius = (7 / 2) - 0.01;
+data.raw["mining-drill"]["kr-electric-mining-drill-mk3"].resource_searching_radius = (9 / 2) - 0.01;
